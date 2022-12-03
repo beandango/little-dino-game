f5d7c5b8cdbaf82046573eea1c5d96ed8efcc84f
Sarah Hughes <106944445+beandango@users.noreply.github.com>
2022-12-03 00:46:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var character = 
+document.getElementById("character");
+var block = 
+document.getElementById("block");
+function jump(){
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500)
+}
+
+var checkDead = setInterval(function(){
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+    var blockLeft = 
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft < 50 && blockLeft > 0 && characterTop >= 110){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("you suck");
+    }
+
+},10);
